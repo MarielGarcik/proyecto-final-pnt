dbf5de3dab4b18ae0370f59c7df382027ab8ecdf
ymsoroco <yaelsoroco@gmail.com>
2025-05-26 23:40:03 -0300

    Varios

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using ContarlaParaVivir.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ContarlaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
