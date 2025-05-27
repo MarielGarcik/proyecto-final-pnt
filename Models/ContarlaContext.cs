ee8baf1733f2e24b0ea6d76f6343d84a803cc601
ymsoroco <yaelsoroco@gmail.com>
2025-05-26 23:39:22 -0300

    Models

---
+++ Models/ContarlaContext.cs
@@ -0,0 +1,17 @@
+﻿using Contarla_Para_Vivir_PNT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContarlaParaVivir.Models
+{
+    public class ContarlaContext : DbContext
+    {
+        public ContarlaContext(DbContextOptions<ContarlaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<DosisLiteraria> DosisLiterarias { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace ContarlaParaVivir.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Comentario { get; set; }
+        public int Puntaje { get; set; } 
+    }
+}
