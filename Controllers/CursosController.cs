b59263d117bdb67148542501afdbd663b39afd56
ymsoroco <yaelsoroco@gmail.com>
2025-06-23 22:42:14 -0300

    Cambios hechos el 23/

--- Controllers/CursosController.cs
+++ Controllers/CursosController.cs
@@ -1,47 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Contarla_Para_Vivir_PNT.Models;
+using ContarlaParaVivir.Models;
 
 namespace Contarla_Para_Vivir_PNT.Controllers
 {
     public class CursosController : Controller
     {
+        private readonly ContarlaContext _context;
+
+        public CursosController(ContarlaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            // Datos temporales simulados (hasta tener SQL)
-            var cursos = new List<Curso>
-            {
-                new Curso { Id = 1, Titulo = "Oratoria inicial", Descripcion = "Perder el miedo a hablar", Nivel = "Principiante", Modalidad = "Zoom" },
-                new Curso { Id = 2, Titulo = "Storytelling emocional", Descripcion = "Conectá con tu audiencia", Nivel = "Intermedio", Modalidad = "Grabado" },
-                new Curso { Id = 3, Titulo = "Mentoría intensiva", Descripcion = "Trabajo uno a uno", Nivel = "Avanzado", Modalidad = "Asesoría" }
-            };
-
+            var cursos = _context.Cursos.ToList(); // ✔️ Ahora carga los cursos reales de la base
             return View(cursos);
         }
-        public IActionResult Mentorias()
-        {
-            return View();
-        }
-
-        public IActionResult Oratoria()
-        {
-            return View();
-        }
-
-        public IActionResult TalleresLiterarios()
-        {
-            return View();
-        }
-
-        public IActionResult Empresarte()
-        {
-            return View();
-        }
-
-        public IActionResult DosisLiterarias()
-        {
-            return View();
-        }
-
-
     }
 }
