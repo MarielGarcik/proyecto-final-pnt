569d5864d6926e243a74c7fde5a7e3331b041fba
Mariel Garcik <mgarcik@gmail.com>
2025-06-28 22:41:06 -0400

    Cambios al 28/06 ready

--- Models/Review.cs
+++ Models/Review.cs
@@ -1,10 +1,14 @@
-﻿namespace ContarlaParaVivir.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContarlaParaVivir.Models
 {
     public class Review
     {
         public int Id { get; set; }
         public string NombreUsuario { get; set; }
         public string Comentario { get; set; }
-        public int Puntaje { get; set; } 
+       
+        [Range(0, 10, ErrorMessage = "El puntaje debe estar entre 0 y 10.")]
+        public int Puntaje { get; set; }
     }
 }
